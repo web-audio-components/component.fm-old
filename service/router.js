c8e4b3d6a144f4a49ba30d9ddb9ba5d00eda72c6
Jordan Santell <jsantell@gmail.com>
2012-11-09 20:13:33 -0500

    Remove subdomain from package.json for service, add default route to service for debugging

--- service/router.js
+++ service/router.js
@@ -2,6 +2,10 @@ module.exports = function ( app ) {
   var
     packages = require( './routes/packages' );
 
+  app.get( '/', function ( req, res ) {
+    res.json({ message: 'WAPM home' });
+  });
+
   app.get( '/packages', packages.index );
   app.post( '/packages', packages.create );
   app.get( '/packages/:name', packages.show );

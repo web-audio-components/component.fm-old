de2d684372beec88b569a603b00df5242a88e276
Jordan Santell <jsantell@gmail.com>
2012-11-09 20:10:28 -0500

    Add mongoDB from jitsu URL

--- service/config.js
+++ service/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db : {
-    URL : 'mongodb://localhost/wapm',
+    URL : 'mongodb://nodejitsu_nko3-swagger:m47p841rljvc2ihuvu5fq5eu7q@ds039257.mongolab.com:39257/nodejitsu_nko3-swagger_nodejitsudb4753732285',
     port : 3000
   },
   port : 8000

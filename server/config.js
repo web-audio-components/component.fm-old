fdd699171be4c150d0cd70dcc50d925f7b33e1e3
Jordan Santell <jsantell@gmail.com>
2012-11-26 19:10:23 -0500

    Remove database components

--- server/config.js
+++ server/config.js
@@ -1,23 +1,14 @@
 var config = {
   production : {
-    db : {
-      URL : process.env['wapm-app-mongodb-url']
-    },
     port : 80,
     serviceURL : 'http://wapm-service.jit.su'
   },
   development : {
-    db : {
-      URL : 'mongodb://localhost:27017/wapm-app'
-    },
     port : 9000,
     serviceURL : 'http://wapm-service.jit.su'
     //serviceURL : 'http://localhost:8000'
   },
   test : {
-    db : {
-      URL : 'mongodb://localhost:27017/wapm-app-test'
-    },
     port : 9000
   } 
 };

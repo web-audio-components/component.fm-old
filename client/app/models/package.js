48c27bbb46634c75d02bcfb60f89b327232fc913
Jordan Santell <jsantell@gmail.com>
2012-11-26 19:59:18 -0500

    Use client-side browsing for modules

--- client/app/models/package.js
+++ client/app/models/package.js
@@ -1,7 +1,35 @@
 (function () {
   app.models.Package = Backbone.Model.extend({ 
+    
     initialize : function () {
 
+    },
+
+    // TODO super inefficient search -- probably should cut down
+    // on search scope
+    matches : function ( query ) {
+      var
+        params = query.split(' '),
+        valid = false,
+        pkg = this;
+
+      params = _.map( params, function ( param ) {
+        return new RegExp( param, 'gi' );
+      });
+
+      _.each( params, function ( param ) {
+        if (
+          _.any( pkg.get('keywords') || [], function ( keyword ) {
+            return keyword.match( param );
+          }) ||
+          pkg.get('description').match( param ) ||
+          pkg.get('name').match( param )
+        ) {
+          valid = true;
+        }
+      });
+
+      return valid;
     }
   });
 })();

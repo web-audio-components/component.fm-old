56455f4e42254001a610faa3765bb4ff3bf3c57c
Jordan Santell <jsantell@gmail.com>
2012-12-08 18:48:35 -0500

    Update player with play/pause controls

--- client/app/views/player.js
+++ client/app/views/player.js
@@ -5,25 +5,30 @@
 
     className : 'player-view',
 
+    events: {
+      'change .samples' : 'handleLoadSample',
+      'click .play-button' : 'handlePlayPause'
+    },
+
     initialize: function ( options ) {
       this.packageView = options.packageView;
-      this.context = new ( window.AudioContext || window.webkitAudioContext )();
+      this.context = options.context;
 
       this.packageView.on( 'activate:demo', this.selectPackage, this );
 
       this.$el.data( 'hidden', true );
-      this.$('.samples').change();
       this.render();
-    },
 
-    events: {
-      'change .samples' : 'handleLoadSample'
+      this.$play = this.$el.find( '.play-button' );
+      this.loadSample( 'vox' );
     },
 
     selectPackage : function ( pkg ) {
       this.package = pkg;
       this.loadPackage();
     },
+
+    // Package
 
     loadPackage : function () {
       var that = this;
@@ -32,6 +37,8 @@
         this.showPlayer();
       }
 
+      this.showLoading();
+      
       require(['/packages/' + this.package.get( 'name' ) + '/script.js'], function ( module ) {
         that.module = new module( that.context );
         that.packageLoaded();
@@ -40,38 +47,59 @@
     },
 
     packageLoaded : function () {
-
+      if ( this.source ) {
+        this.source.connect( this.module.input );
+      }
+      this.hideLoading();
     },
 
-    handleLoadSample : function ( e ) {
-      var sample = $( e.target ).find(':selected').val();
-      this.loadSample( sample );
-    },
+    // Sample
 
     loadSample : function ( sample ) {
       var that = this;
 
-      // If previous buffer connected and playing, kill it
-      if ( this.source && this.source.noteOff ) {
-        this.source.noteOff( 0 );
-      }
+      this.showLoading();
+      this.removeSample();
 
       this.source = this.context.createBufferSource();
-      var xhr = new XMLHttpRequest();
-      xhr.open( 'GET', 'samples/' + sample + '.mp3', true );
-      xhr.responseType = 'arraybuffer';
-      xhr.onload = function ( buffer ) {
-        that.loadedSample( buffer, xhr );
-      };
-      xhr.send();
+      allen.getBuffer( 'samples/' + sample + '.mp3', function ( xhr ) {
+        that.loadedSample( xhr.target.response );
+      });
     },
 
-    loadedSample : function ( buffer, xhr ) {
-      this.source.buffer = this.context.createBuffer( xhr.response, false );
+    loadedSample : function ( buffer ) {
+      this.source.buffer = this.context.createBuffer( buffer, false );
       if ( this.module ) {
         this.source.connect( this.module.input );
       }
+      this.hideLoading();
+    },
+
+    removeSample : function () {
+      if ( this.source && this.source.disconnect ) {
+        this.source.disconnect();
+      }
+      this.stopSample();
+    },
+
+    playSample : function () {
+                   console.log('playsample');
+      this.$play.data( 'playing', true )
+        .find('i')
+          .removeClass( 'icon-play' )
+          .addClass( 'icon-pause' );
       this.source.noteOn( 0 );
+    },
+
+    stopSample : function () {
+                   console.log('stopsample');
+      this.$play.data( 'playing', false )
+        .find('i')
+          .addClass( 'icon-play' )
+          .removeClass( 'icon-pause' );
+      if ( this.source && this.source.noteOff ) {
+        this.source.noteOff( 0 );
+      }
     },
 
     showPlayer : function () {
@@ -82,6 +110,45 @@
     hidePlayer : function () {
       this.$el.animate({ 'margin-bottom' : '-150px' });
       this.$el.data('hidden', true);
+    },
+
+    // Loading GUI
+
+    showLoading : function () {
+      this.$el
+        .find('.player-loading').show().end()
+        .find('select, a').prop('disabled', true)
+          .addClass('disabled');
+    },
+
+    hideLoading : function () {
+      this.$el
+        .find('.player-loading').hide().end()
+        .find('select, a').prop('disabled', false)
+          .removeClass('disabled');
+    },
+
+    // Event handlers
+
+    handleLoadSample : function ( e ) {
+      var sample = $( e.target ).find(':selected').val();
+      this.loadSample( sample );
+    },
+
+    handlePlayPause : function ( e ) {
+      e.preventDefault();
+
+      // Abort if button has class disabled
+      if ( this.$play.hasClass('disabled') ) {
+        return;
+      }
+
+      if ( this.$play.data( 'playing' ) ) {
+        this.stopSample();
+      } else {
+        this.playSample();
+      }
     }
+
   });
 })();

14c2d82f0ad5156fd2719c6f76fb5d03abf817ad
Jordan Santell <jsantell@gmail.com>
2013-01-27 23:59:24 -0500

    Update resource for audio components, pointing to jit.su api

--- client/app/collections/packages.js
+++ client/app/collections/packages.js
@@ -2,7 +2,7 @@
   app.collections.Packages = Backbone.Collection.extend({
     model : app.models.Package,
 
-    url : '/packages',
+    url : 'http://web-audio-components-api.jit.su/components',
 
     initialize: function () {
 

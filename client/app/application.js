9e6662e9351af2122dc0cd7fbfd7a1c24854997e
Jordan Santell <jsantell@gmail.com>
2013-01-27 23:57:03 -0500

    Main setup to use components, remove requirejs usage

--- client/app/application.js
+++ client/app/application.js
@@ -1,38 +1,44 @@
-app.init = function () {
-  
-  console.log('past');
+/**
+ * Globally expose components
+ */
 
+window.Rack   = require('web-audio-components-rack');
+window.moment = require('component-moment');
+window.allen  = require('jsantell-allen');
+
+/**
+ * Initialize app
+ */
+
+app.init = function () {  
   app.packages = new app.collections.Packages();
   app.context = allen.getAudioContext();
 
   app.searchBarView = new app.views.SearchBar();
+  
   app.searchResultsView = new app.views.SearchResults({
     search : app.searchBarView,
     packages : app.packages
   });
+  
   app.packageView = new app.views.Package({
     search : app.searchBarView,
     resultsView : app.searchResultsView,
     packages : app.packages
   });
+  
   app.playerView = new app.views.Player({
     packageView : app.packageView,
     context : app.context
   });
+
   app.indexView = new app.views.Index({
     packages : app.packages,
     search : app.searchBarView
   });
-
 };
 
 $(function () {
-  // Use requirejs to expose allen, since it won't be global
-  // due to existence of requirejs for pulling in
-  // audio modules
-  require([ 'allen' ], function ( allen ) {
-    window.allen = allen;
-    app.init.call( app );
-    Backbone.history.start();
-  });
+  app.init.call( app );
+  Backbone.history.start();
 });

f86c1529a705fac8a707c362d8dc7c4a0d2dcb36
Jordan Santell <jsantell@gmail.com>
2013-01-31 20:00:50 -0500

    Even more streamlined server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require('./server/server.js');
+var
+  express = require('express'),
+  app = express(),
+  port = 9000;
+
+app
+  .use(express.bodyParser())
+  .use(express.static(__dirname + '/client/public/'))
+  .use(app.router);
+
+app.set('views', __dirname + '../client/public/');
+app.engine('html', require('ejs').renderFile);
+
+app.listen(port);
+
+console.log('Web Audio Components client running');
+console.log('  Port: ' + port);
+
+module.exports = app;
